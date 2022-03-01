16a7c5b223b37576951a316c71aed4c69bb79023
Mathieu Delcambre <delcmabre.mathieu@gmail.com>
2022-03-01 15:09:49 +0100

    BDD Foutu projet poubelle

---
+++ database/migrations/2022_03_01_135620_create__ecommerce_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('_ecommerce', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->text('description');
+            $table->integer('prix');
+            $table->string('slug');
+            $table->timestamp('release_at')->nullable();
+            $table->string('cdc');
+            $table->string('colorlist');
+            $table->string('image');
+            $table->integer('promotion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_ecommerce');
+    }
+};
